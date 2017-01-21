547906a9234981b2f6950f2f7e8b36392775ad25
zacks <zackspbc@gmail.com>
2017-01-21 10:43:17 +0800

    add scripts

---
+++ BirdManipulator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdManipulator : MonoBehaviour {
+    public GameObject bird;
+    private Vector3 position = new Vector3(-2.5f, 0, 0);
+
+	// Use this for initialization
+	void Start () {
+	    GetComponent<MicrophoneRecorder>().onMicrophone += MoveBird;
+	}
+
+    private void MoveBird(float value) {
+        float yVelocity = 0;
+        float newPosition = Mathf.SmoothDamp(transform.position.y, value / 100 - 5, ref yVelocity, 0.3f);
+        position.y = Mathf.Lerp(position.y, value / 100 - 5, 0.3f);
+//        position.y = value /100 - 5;
+        bird.transform.position = position;
+    }
+}
---
+++ Test/AudioTest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class AudioTest : MonoBehaviour
+{
+	
+}
